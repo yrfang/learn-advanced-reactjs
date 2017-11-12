cd35e4685241a01d628576086357e44593bba4c5
yrfang <fyr1116@gmail.com>
2017-11-12 15:39:43 +0800

    Adding reducer and its new state depends on action for comment_box

--- testing/test/reducers/comments_test.js
+++ testing/test/reducers/comments_test.js
@@ -4,12 +4,11 @@ import { SAVE_COMMENT } from '../../src/actions/types';
 
 describe('Comment Reducer', () => {
   it('handles action with unknown type', () => {
-    expect(commentReducer()).to.be.instance(Array);
-    expect(commentReducer()).to.eql([]);
+    expect(commentReducer(undefined, {})).to.eql([]);
   });
 
   it('handle action of type SAVE_COMMENT', () => {
-    const action = {  type:SAVE_COMMENT, payload: 'new comment' };
+    const action = {  type: SAVE_COMMENT, payload: 'new comment' };
     expect(commentReducer([], action)).to.eql(['new comment']);
   });
 });
