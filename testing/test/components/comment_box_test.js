c765866c437a1364b46c1bd7e03eed147dc70fcd
yrfang <fyr1116@gmail.com>
2017-11-12 13:58:40 +0800

    simulating the change event in textarea for comment_box component with testing

--- testing/test/components/comment_box_test.js
+++ testing/test/components/comment_box_test.js
@@ -19,4 +19,19 @@ describe('CommentBox Test', () => {
   it('has a button', () => {
     expect(component.find('button')).to.exist;
   });
+
+  describe('entering some text', () => {
+    beforeEach(() => {
+      component.find('textarea').simulate('change', 'new comment');
+    });
+    
+    it('shows that text in the textarea', () => {
+      expect(component.find('textarea')).to.have.value('new comment');
+    });
+
+    it('when submitted, clears the input', () => {
+
+    });
+  });
+
 });
