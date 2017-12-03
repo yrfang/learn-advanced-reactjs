a364ab091c7fee1264f5e7d19ef971618d12e78e
yrfang <fyr1116@gmail.com>
2017-12-03 20:19:15 +0800

    Set up testing environment to run like a browser in the command line

--- testing/test/test_helper.js
+++ testing/test/test_helper.js
@@ -1,13 +1,15 @@
+import jsdom from 'jsdom';
+import jquery from 'jquery';
+
+// Set up testing environment to run like a browser in the command line
+global.document = jsdom.jsdom('<!doctype html><html><body></body></html>');
+global.window = global.document.defaultView;
+const $ = jquery(global.window);
+
+// build 'renderComponent' helper that should render a given react class
 
 
-
-//Set up testing environment to run like a browser in the command line
-
-
-//build 'renderComponent' helper that should render a given react class
+// Build helper for simulating events
 
 
-//Build helper for simulating events
-
-
-//Set up chai-jquery
+// Set up chai-jquery
