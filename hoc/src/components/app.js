557d9627980d15ab0e725ded7c0451420e243035
yrfang <fyr1116@gmail.com>
2017-12-05 23:13:39 +0800

    react router setup

--- hoc/src/components/app.js
+++ hoc/src/components/app.js
@@ -1,4 +1,5 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { Component } from 'react';
 
 import Header from './header';
 
@@ -7,6 +8,7 @@ export default class App extends Component {
     return (
       <div>
         <Header />
+        {this.props.children}
       </div>
     );
   }
