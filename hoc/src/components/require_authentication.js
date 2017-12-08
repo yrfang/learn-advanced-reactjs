0026fc05f76c50fc4e335a6ac60d2177510bcc52
yrfang <fyr1116@gmail.com>
2017-12-08 16:42:25 +0800

    handling HOC edge cases

--- hoc/src/components/require_authentication.js
+++ hoc/src/components/require_authentication.js
@@ -7,9 +7,21 @@ export default function(ComposedComponent) {
       router: React.PropTypes.object
     }
 
+    componentWillMount() {
+      if (!this.props.authenticated) {
+        this.context.router.push('/');
+      }
+    }
+
+    componentWillUpdate(nextProps) {
+      if (!nextProps.authenticated) {
+        this.context.router.push('/');
+      }
+    }
+
     render () {
-      console.log(this.context);
-      console.log(this.props.authenticated);
+      // console.log(this.context);
+      // console.log(this.props.authenticated);
       return <ComposedComponent {...this.props} />
     }
   }
