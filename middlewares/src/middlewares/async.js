7e31be47e92739539739cdaa02c35acdf8945043
YR Fang <fyr1116@gmail.com>
2018-01-07 19:41:41 +0800

    Handling unrelated actions

--- middlewares/src/middlewares/async.js
+++ middlewares/src/middlewares/async.js
@@ -1,8 +1,14 @@
-
 export default function({ dispatch }) {
   return next => action => {
-    console.log(action);
+    // console.log(action);
 
-    next(action);
+    // If action does not have payload
+    // or, the payload does not have a .then property
+    // we dont care about it, send it on
+    if (!action.payload || !action.payload.then) {
+      return next(action);
+    }
+
+    console.log('we dont have a promise', action);
   }
 }
