54fed9f4cccc85cac4524dbab1672637830bfc5e
YR Fang <fyr1116@gmail.com>
2018-01-14 12:26:14 +0800

    Adding Header component in app

---
+++ auth/client/src/components/header.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router';
+
+class Header extends Component {
+
+  render() {
+    return (
+      <nav className="navbar navbar-light">
+        <ul className="nav navbar-nav">
+          <li className="nav-item">Sign in</li>
+        </ul>
+      </nav>
+    );
+  }
+}
+
+export default Header;
